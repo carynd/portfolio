c6b283c005da8160d3eb1d10b276c170b7095eb0
Caryn D <54146731+carynd@users.noreply.github.com>
2023-04-25 12:38:31 +0530

    added changes

--- index.js
+++ index.js
@@ -42,3 +42,55 @@
     }
   });
   
+  const sentences = [  "Java Backend Engineer.",  "Software Engineer."];
+
+const textElement = document.querySelector('.text');
+let sentenceIndex = 0;
+let letterIndex = 0;
+
+function typeSentence() {
+  if (letterIndex < sentences[sentenceIndex].length) {
+    textElement.textContent += sentences[sentenceIndex][letterIndex];
+    letterIndex++;
+    setTimeout(typeSentence, 50);
+  } else {
+    setTimeout(eraseSentence, 2000);
+  }
+}
+
+function eraseSentence() {
+  if (letterIndex > 0) {
+    textElement.textContent = sentences[sentenceIndex].substring(0, letterIndex - 1);
+    letterIndex--;
+    setTimeout(eraseSentence, 30);
+  } else {
+    sentenceIndex++;
+    if (sentenceIndex >= sentences.length) {
+      sentenceIndex = 0;
+    }
+    setTimeout(typeSentence, 500);
+  }
+}
+
+typeSentence();
+const myButton = document.getElementById("nav-link");
+let clicked = false;
+
+myButton.addEventListener("click", () => {
+  if (!clicked) {
+    myButton.style.backgroundColor = "pink";
+    clicked = true;
+  } else {
+    myButton.style.backgroundColor = "white";
+    clicked = false;
+  }
+});
+const navLinks = document.querySelectorAll('.nav_link');
+
+navLinks.forEach(navLink => {
+  navLink.addEventListener('click', (event) => {
+    event.preventDefault();
+    navLinks.forEach(link => link.classList.remove('active'));
+    event.target.classList.add('active');
+  });
+});
